c3873687c1f43add28ad2bd47cbf35ab5042ea77
Julian Frank <jfempire@yahoo.co.uk>
2017-11-12 06:00:16 +0530

    json back to go structure issue!

--- gungo.go
+++ gungo.go
@@ -2,31 +2,55 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+
 	"log"
 )
 
-type test struct {
-	Item1 interface{}
+//GunGraph DB Instance
+type GunGraph struct {
+	Graph map[string]interface{}
 }
 
 func main() {
 
-	fmt.Print("fmt")
+	//Instantiating the DB here
+	var db GunGraph
+	db.Graph = make(map[string]interface{})
 
-	log.Print("log")
+	//Creating the First Node here
+	n1ID := make(map[string]string)
+	n1ID["#"] = "ASDF"
+	n1Node := make(map[string]interface{})
+	n1Node["_"] = n1ID
+	n1Node["Name"] = "JF"
+	//Creating the Second Node here
+	n2ID := make(map[string]string)
+	n2ID["#"] = "FDSA"
+	n2Node := make(map[string]interface{})
+	n2Node["_"] = n2ID
+	n2Node["Name"] = "Fluffy"
+	n2Node["Species"] = "Cat"
+	//Connecting both nodes here
+	n1Node["boss"] = n2ID
+	n2Node["slave"] = n1ID
+	//Adding the nodes to the DB
+	db.Graph["ASDF"] = n1Node
+	db.Graph["FDSA"] = n2Node
 
-	x := test{Item1: "gsdfgs"}
+	//Trying out Traversals
+	log.Print("db.Graph[ASDF]\t", db.Graph["ASDF"])
+	t := db.Graph["ASDF"].(map[string]interface{})
+	boss := t["boss"].(map[string]string)
+	log.Print("n1 boss\t", t["boss"], db.Graph[boss["#"]])
 
-	log.Print(x)
-
-	m := make(map[interface{}]interface{})
-
-	m["_"] = "hello"
-
-	log.Print(m)
-
-	myjson, err := json.Marshal(m)
-
-	log.Print(myjson, err)
+	//Converting DB to JSON for communication with Peers
+	dbjson, err := json.Marshal(db)
+	log.Print("string(dbjson), err\t", string(dbjson), err)
+	//Checking Reverse MApping back to DB from json format
+	tempDB := db
+	err = json.Unmarshal(dbjson, &tempDB)
+	log.Printf("tempDB \tType:%T\tValue:%v", tempDB, tempDB)
+	//Trying Out Traversal
+	//db.Graph = tempDB.Graph.(map[string]interface{})
+	//log.Print(db.Graph["ASDF"]["boss"])
 }
